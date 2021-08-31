b157fd9b139ef6665a6b343dd57a1d1cd4e316d2
Suraj Kumar <sk9966332@gmail.com>
2021-08-31 18:27:46 +0530

    social media app

---
+++ social/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now=True)
+    content = models.TextField()
+    image = models.ImageField()
+
+    def __str__(self):
+        return self.content[:50]
+
+class Friend(models.Model):
+    person1 = models.ForeignKey(User, on_delete=models.CASCADE, related_name='person1')
+    person2 = models.ForeignKey(User, on_delete=models.CASCADE, related_name='person2')
